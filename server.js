661a5995d33027711cffcbe24ce91d7846efe79f
Thanikul Sriuthis <dekitclub@gmail.com>
2020-11-10 14:15:09 +0700

    keep data.txt

--- server.js
+++ server.js
@@ -1,22 +1,22 @@
-var express = require ('express'), fs = require ('fs');
-url = require ('url');
-var app = express ();
+const express = require('express')
+const fs = require('fs')
+const app = express()
 
-app.use ('/json', express.static (__dirname + '/json'));
-app.use (express.static (__dirname + '/json'));
+app.use('/token', express.static(__dirname + '/token'))
+app.use(express.static(__dirname + '/token'))
 
-app.post ('/receive', function (request, respond) {
-  var body = '';
-  filePath = __dirname + '/json/data.txt';
-  request.on ('data', function (data) {
-    body += data;
-  });
+app.post('/token', function (request, respond) {
+  var body = ''
+  filePath = __dirname + '/token/data.txt'
+  request.on('data', function (data) {
+    body = data
+  })
 
-  request.on ('end', function () {
-    fs.writeFile (filePath, body, function () {
-      respond.end ();
-    });
-  });
-});
+  request.on('end', function () {
+    fs.writeFile(filePath, body, function () {
+      respond.end()
+    })
+  })
+})
 
-app.listen (8009);
+app.listen(8009)
