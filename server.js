cac47ccaff580a24b9c92e5b3b27b11bb5f3875e
Thanikul Sriuthis <dekitclub@gmail.com>
2020-11-10 19:00:00 +0700

    add get request

--- server.js
+++ server.js
@@ -2,21 +2,26 @@ const express = require('express')
 const fs = require('fs')
 const app = express()
 
+const filePath = __dirname + '/token/data.txt'
+
 app.use('/token', express.static(__dirname + '/token'))
 app.use(express.static(__dirname + '/token'))
 
-app.post('/token', function (request, respond) {
-  var body = ''
-  filePath = __dirname + '/token/data.txt'
-  request.on('data', function (data) {
+app.post('/token', (req, res) => {
+  let body = ''
+  req.on('data', data => {
     body = data
   })
 
-  request.on('end', function () {
-    fs.writeFile(filePath, body, function () {
-      respond.end()
+  req.on('end', () => {
+    fs.writeFile(filePath, body, () => {
+      res.end()
     })
   })
 })
 
+app.get('/token', (req, res) => {
+  let token = fs.readFileSync(filePath)
+  console.log(`${token}`)
+})
 app.listen(8009)
